8db0e78e9e4051a18d93a68434cce12214b47f27
bipul <bk70937@gmail.com>
2021-12-10 14:09:32 +0530

    env

--- mongoDB.js
+++ mongoDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 // const connectDatabase = () => {
 //     mongoose.connect('mongodb://localhost:27017/wizard', {
 //         useNewUrlParser: true,
@@ -13,8 +13,9 @@ const mongoose = require('mongoose')
 
 // mongodb+srv://bk70937:Bip#70937@marketplace.cy4ct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
+const url = process.env.MONGODB_URI
 const connectDatabase = () => {
-    mongoose.connect('mongodb+srv://bk70937:Bip%2370937@marketplace.cy4ct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/marketplace', {
+    mongoose.connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then((data) => {
