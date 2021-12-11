1b98e9568b0fc757a12ded4d528dd5c2f571b1a6
bipul <bk70937@gmail.com>
2021-12-11 16:46:42 +0530

    majorchanges

---
+++ models/undeaddata.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const UndeadDataSchema = mongoose.Schema({
+    buyer: { type: String },
+    entTimeStamp: { type: String },
+    highestBid: { type: String },
+    highestBidder: { type: String },
+    id: { type: Number },
+    isAuction: { type: Boolean },
+    lister: { type: String },
+    nftAdr: { type: String },
+    nftTokenId: { type:  String },
+    nftTokenPrice: { type: String },
+    openTimeStamp: { type: String },
+    priceTokenAdr: { type: String },
+    title: { type: String },
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('UndeadData', UndeadDataSchema)
--- routes.js
+++ routes.js
@@ -7,7 +7,10 @@ const Web3 = require('web3');
 // MongoDB
 const mongoose = require('mongoose');
 const Wizard = require('./models/wizard')
+const WizardData = require('./models/wizarddata')
+
 const Undead = require('./models/undead')
+const UndeadData = require('./models/undeaddata')
 
 const rpcUrl = 'https://rpc.ftm.tools:443/';
 
@@ -17,7 +20,8 @@ const address_wizard = "0x7B1294C50758ed380C1ff9295fFB1797e33DE7E7"
 const MARKETPLACE_ABI = require('./config/MARKETPLACE_ABI.json')
 const address = "0x64eF011168188ccdA22b4d70D146Fb019906bAF6";
 
-const WARRIOR_ABI = require('./config/WARRIOR_ABI.json')
+const WARRIOR_ABI = require('./config/WARRIOR_ABI.json');
+const { isNull } = require('lodash');
 const warrior_address = '0x752C62a41713BB8c95dE7e4551195059be3bDA63'
 
 // Database Connection
@@ -59,7 +63,6 @@ router.get('/api/marketplace/add/undead', async (req,res,next) => {
                 instant : instant.join(','),
             };
 
-            console.log(onsale)
 
             const wizard = await Wizard.create(data)
 
@@ -101,7 +104,6 @@ router.get('/api/marketplace/add/wizard', async (req,res,next) => {
         for(let i = 0 ; i < count ; i++ ) {
     
         let _statusF =  await contract.methods.getFullTrade(i).call() ;
-        console.log(_statusF)
         if(_statusF[8]){
           let _status =  await contract.methods.getAuctionStatus(i).call() ;
           if(_status == 1){
@@ -151,38 +153,136 @@ router.get('/api/marketplace/wizard', async (req, res, next) => {
          
 })
 
-router.get('/api/marketplace/wizarddata', async (req, res, next) => {
+
+// wizard data
+router.get('/api/marketplace/add/wizarddata', async (req, res, next) => {
+    try {
+        const web3 = new Web3(rpcUrl)
+        const contract = new web3.eth.Contract(MARKETPLACE_ABI, address)
+        let count = await contract.methods.getTradeCount().call()
+    
+        for(let i = 0 ; i < count ; i++ ) {
+    
+            let _statusF =  await contract.methods.getFullTrade(i).call() ;
+
+            let data = {
+                buyer : _statusF.buyer,
+                entTimeStamp : _statusF.entTimeStamp,
+                highestBid : _statusF.highestBid,
+                highestBidder : _statusF.highestBidder,
+                id : _statusF.id,
+                isAuction : _statusF.isAuction,
+                lister : _statusF.lister,
+                nftAdr : _statusF.nftAdr,
+                nftTokenId : _statusF.nftTokenId,
+                nftTokenPrice : _statusF.nftTokenPrice,
+                nftTokenPrice : _statusF.nftTokenPrice,
+                openTimeStamp : _statusF.openTimeStamp,
+                priceTokenAdr : _statusF.priceTokenAdr,
+                title : _statusF.title,
+            }
+            WizardData.find( {  "id" : { $eq : parseInt(_statusF.id)} }, async (err, object) => {
+                if (object.length > 0 ) {
+                    let _result = await WizardData.updateOne( {"id": object[0].id} , { $set:data}, { multi: true }, );
+                } else {
+                   await WizardData.create (data)
+                }
+            });
+        }
+        res.json({ message: 'Data Updated Successfully' })
+
+      } catch(err){
+        next(err) 
+      } 
+
+})
+
+router.get('/api/marketplace/list/wizarddata', async (req, res, next) => {
+    try {
+        const wizard = await WizardData.find()
+        if(wizard.length > 0){
+            return res.status(200).json(wizard);
+        }
+    } catch(err){
+        next(err);
+    }   
+})
+
+router.get('/api/marketplace/lister/wizarddata', async (req, res, next) => {
+    try {
+        const wizard = await WizardData.find({"lister":{$exists:true}})
+        if(wizard.length > 0){
+            return res.status(200).json(wizard);
+        }
+    } catch(err){
+        next(err);
+    } 
+})
+
+
+// undead data
+router.get('/api/marketplace/add/undeaddata', async (req, res, next) => {
     try {
         const web3 = new Web3(RPC_URL)
         const contract = new web3.eth.Contract(MARKETPLACE_ABI, address_wizard)
         let count = await contract.methods.getTradeCount().call()
     
-        let onsale = [];
-        let sold = [];
-        let instant = [];
-    
         for(let i = 0 ; i < count ; i++ ) {
     
-        let _statusF =  await contract.methods.getFullTrade(i).call() ;
-        return res.json(_statusF)
-        // if(i == (count-1)) {
-        //     let data = {
-        //         onsale : onsale.join(','),
-        //         sold : sold.join(','),
-        //         instant : instant.join(','),
-        //     };
-        //     const undead = await Undead.create(data)
+            let _statusF =  await contract.methods.getFullTrade(i).call() ;
 
-        //     res.status(201).json({
-        //         message: 'Data Inserted Successfully'
-        //     })
-        // }
+            let data = {
+                buyer : _statusF.buyer,
+                entTimeStamp : _statusF.entTimeStamp,
+                highestBid : _statusF.highestBid,
+                highestBidder : _statusF.highestBidder,
+                id : _statusF.id,
+                isAuction : _statusF.isAuction,
+                lister : _statusF.lister,
+                nftAdr : _statusF.nftAdr,
+                nftTokenId : _statusF.nftTokenId,
+                nftTokenPrice : _statusF.nftTokenPrice,
+                nftTokenPrice : _statusF.nftTokenPrice,
+                openTimeStamp : _statusF.openTimeStamp,
+                priceTokenAdr : _statusF.priceTokenAdr,
+                title : _statusF.title,
+            }
+            UndeadData.find( {  "id" : { $eq : parseInt(_statusF.id)} }, async (err, object) => {
+                if (object.length > 0 ) {
+                    let _result = await UndeadData.updateOne( {"id": object[0].id} , { $set:data}, { multi: true }, );
+                } else {
+                   await UndeadData.create (data)
+                }
+            });
         }
+        res.json({ message: 'Data Updated Successfully' })
+
       } catch(err){
         next(err) 
       } 
 })
 
+router.get('/api/marketplace/list/undeaddata', async (req, res, next) => {
+    try {
+        const undead = await UndeadData.find()
+        if(undead.length > 0){
+            return res.status(200).json(undead);
+        }
+    } catch(err){
+        next(err);
+    }   
+})
+
+router.get('/api/marketplace/lister/undeaddata', async (req, res, next) => {
+    try {
+        const undead = await UndeadData.find({"lister":{$exists:true}})
+        if(undead.length > 0){
+            return res.status(200).json(undead);
+        }
+    } catch(err){
+        next(err);
+    } 
+})
 
 
 
@@ -197,8 +297,6 @@ router.get('/api/test/:start/:end', async  (req, res, next) => {
         //   return events;
           return res.json(events)
        })
-
- 
 })
 
 module.exports = router;
